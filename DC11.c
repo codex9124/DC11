8f7be4f5d099288147ad7c8018d4495d67f5de5e
codex9124 <akhade.avi.dhule@gmail.com>
2025-11-09 22:18:37 +0530

    Create DC11.c

---
+++ DC11.c
@@ -0,0 +1,25 @@
+clear,
+
+dc
+
+clear;
+
+p * 1 = 1/2
+
+p * 2 = 1/4
+
+px3-1/8;
+
+px * 4 = 1/16 ;
+
+p * 5 = 1/16
+
+Tb = 10 deg - 3
+
+HXlog2(1/px1)+px2*log2(1/px2)+px3*log2(1/px3)+px4*log2(1/px4)+px5*log2(1/px5); printf("i) source entropy\n\n \tH(X) = %.2f bits/symbol\n", HX);//source entropy
+
+r = 1 / T * b
+
+Rer*HX://information rate
+
+printf("\n\n ii) information rate \n\n \t R = %d bits/sec",R);
